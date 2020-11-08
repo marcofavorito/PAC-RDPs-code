a8dc6e60cdda871289c7852f57ea28bc2b2612d5
MarcoFavorito <marco.favorito@gmail.com>
2020-11-08 14:58:48 +0100

    add tests for PAC RDP

--- tests/test_pac_rdp.py
+++ tests/test_pac_rdp.py
@@ -50,8 +50,8 @@ def learning_rotating_mab(
     return rdp_generator, (v, t)
 
 
-def test_learning_rotating_mab():
-    """Test learning rotating MAB."""
+def test_learning_rotating_mab_2_arms_nondeterministic():
+    """Test learning rotating MAB with 2 arms, nondeterministic."""
     rdp_generator, (v, t) = learning_rotating_mab(
         stop_probability=0.2,
         winning_probabilities=(0.7, 0.3),
@@ -62,3 +62,47 @@ def test_learning_rotating_mab():
         nb_processes=8,
     )
     assert len(v) == 2
+
+
+def test_learning_rotating_mab_3_arms_deterministic():
+    """Test learning rotating MAB with 3 arms, deterministic."""
+    rdp_generator, (v, t) = learning_rotating_mab(
+        stop_probability=0.2,
+        winning_probabilities=(1.0, 0.0, 0.0),
+        max_episode_steps=15,
+        nb_samples=100000,
+        delta=0.1,
+        n_upperbound=5,
+        nb_processes=8,
+    )
+
+    assert len(v) == 3
+
+
+def test_learning_rotating_mab_3_arms_nondeterministic():
+    """Test learning rotating MAB with 3 arms, nondeterministic."""
+    rdp_generator, (v, t) = learning_rotating_mab(
+        stop_probability=0.2,
+        winning_probabilities=(0.1, 0.2, 0.9),
+        max_episode_steps=1000,
+        nb_samples=1000000,
+        delta=0.05,
+        n_upperbound=6,
+        nb_processes=8,
+    )
+
+    assert len(v) == 3
+
+
+def test_learning_rotating_mab_4_arms_deterministic():
+    """Test learning rotating MAB with 4 arms, deterministic."""
+    rdp_generator, (v, t) = learning_rotating_mab(
+        stop_probability=0.2,
+        winning_probabilities=(1.0, 0.0, 0.0, 0.0),
+        max_episode_steps=50,
+        nb_samples=300000,
+        delta=0.1,
+        n_upperbound=5,
+        nb_processes=8,
+    )
+    assert len(v) == 4
