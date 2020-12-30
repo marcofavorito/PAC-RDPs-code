f9a281cecedb438ade59fc79fab44f196d213129
MarcoFavorito <marco.favorito@gmail.com>
2020-12-30 23:24:51 +0100

    update tox.ini

--- scripts/configure_env.sh
+++ scripts/configure_env.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-function install_poetry_dependency(){
-    cd "$1"
-    poetry build
-    python -m pip install `ls dist/*.whl`
-    cd ..
-}
-
-install_poetry_dependency yarllib
-
+pipenv run pip install yarllib --no-cache-dir --force
 
 set +e
