76dcbbf065710ee361748c0a1d8eb141922d3a58
MarcoFavorito <marco.favorito@gmail.com>
2021-01-10 16:12:09 +0100

    use master branch

--- scripts/run-all-cloud.sh
+++ scripts/run-all-cloud.sh
@@ -4,9 +4,9 @@
 TOKEN="$(cat .github_access_token)"
 git clone https://marcofavorito:${TOKEN}@github.com/whitemech/PAC-RDPs-code.git code
 cd code
-git checkout cloud
+git checkout master
 
 pipenv --python python3.8
 pipenv install --dev --skip-lock
 
-pipenv run scripts/run-all.sh $@
+pipenv run scripts/run-all.sh "$@"
